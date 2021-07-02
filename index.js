fdf07f181f1e23305ca0e8d06cbc4c33dc7a07a7
vlebelleguy <80490764+vlebelleguy@users.noreply.github.com>
2021-07-02 19:59:26 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require("express");
+// PACKAGES IMPORT //
 const cors = require("cors");
+const express = require("express");
 
+// PACKAGES INIT //
 const app = express();
 app.use(cors());
 
@@ -450,17 +452,6 @@ app.get("/", (req, res) => {
     ],
   });
 });
-
-// Heroku va nous fournir une variable process.env.PORT
-// if (process.env.PORT) {
-//   app.listen(process.env.PORT, () => {
-//     console.log("Server started");
-//   });
-// } else {
-//   app.listen(3200, () => {
-//     console.log("Server started");
-//   });
-// }
 
 app.listen(process.env.PORT || 3200, () => {
   console.log("Server started");
